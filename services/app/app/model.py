b010b6899d9bebe04f0b162caa5199cea256118e
JTC <17030942+JialunC@users.noreply.github.com>
2021-01-01 16:44:37 -0500

    models

--- services/app/app/model.py
+++ services/app/app/model.py
@@ -1,5 +1,6 @@
+import bcrypt
+from datetime import datetime
 from main import db
-from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -8,15 +9,18 @@ class User(db.Model):
     uuid = db.Column(db.String(255), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.LargeBinary())
+    created_on = db.Column(db.DateTime, nullable=True)
 
-    def __init__(self, email):
+    def __init__(self, email, password):
         self.email = email
+        self.set_password(password)
+        self.created_on = datetime.now()
 
     def set_password(self, password):
-        pass
+        self.password_hash = bcrypt.hashpw(password, bcrypt.gensalt())
 
     def check_password(self, password):
-        pass
+        return bcrypt.hashpw(password, self.password_hash) == hashed
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
