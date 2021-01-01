46b6900d9be42d4ad7e793e67702b6ad41cbcc4e
JTC <17030942+JialunC@users.noreply.github.com>
2020-12-31 23:03:37 -0500

    add uuid

--- services/app/app/model.py
+++ services/app/app/model.py
@@ -5,6 +5,7 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
+    uuid = db.Column(db.String(255), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.LargeBinary())
 
